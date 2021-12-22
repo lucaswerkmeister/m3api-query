ddde9e8c47afe5db669aa7d19ff55d6b8914d54d
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-12-22 22:55:47 +0100

    Add getResponsePageByTitle function
    To get the page from an API response while accounting for some
    subtleties. I’ll almost certainly also add a second function that
    directly makes the API request for you, but this comes first. (The
    function name has “by title” because I’ll probably add “by pageid” and
    “by revid” versions too.)

--- index.js
+++ index.js
@@ -1,2 +1,64 @@
+/**
+ * Get the page with the given title out of an API response.
+ *
+ * Accounts for normalized titles and redirects in the response,
+ * which means that the title of the returned page may not be the same as the given title.
+ * Note that the API does not resolve redirects by default,
+ * so that part will only apply if the request was made with the redirects parameter,
+ * otherwise the returned page will be the information about the redirect instead of its target.
+ * (Normalization is always applied, though.)
+ *
+ * This only looks for pages inside the query.pages of the response.
+ * The pages can have been specified using the titles, pageids, revids, or generator parameters,
+ * but titles added by a list parameter will not be found (use the list as a generator instead).
+ *
+ * @param {Object} response A full response, as returned by {@link Session#request}.
+ * @param {string} title The title of the page to look for.
+ * @return {Object|null} The page with the given title,
+ * or null if no such page was found in the response.
+ * If the request was made with the redirects parameter,
+ * then this means that no such non-redirect page was found,
+ * which could be because the title was a redirect to a nonexistent page,
+ * or resulted in a redirect loop.
+ * If the request was *not* made with the redirects parameter,
+ * then a null return is unlikely,
+ * but the returned object might just indicate an invalid or missing title.
+ * Otherwise, the contents of the page object will depend
+ * on the parameters with which the request was made,
+ * especially the prop parameter.
+ */
+function getResponsePageByTitle( response, title ) {
+	for ( const normalized of response.query.normalized || [] ) {
+		if ( title === normalized.from ) {
+			title = normalized.to;
+			break;
+		}
+	}
+
+	const visitedRedirects = new Set();
+	redirectLoop: do {
+		for ( const redirect of response.query.redirects || [] ) {
+			if ( title === redirect.from ) {
+				visitedRedirects.add( redirect.from );
+				title = redirect.to;
+				continue redirectLoop;
+			}
+		}
+		break;
+	} while ( !visitedRedirects.has( title ) );
+
+	let pages = response.query.pages || [];
+	if ( !Array.isArray( pages ) ) {
+		pages = Object.values( pages );
+	}
+	for ( const page of pages ) {
+		if ( title === page.title ) {
+			return page;
+		}
+	}
+	return null;
+}
+
 export {
+	getResponsePageByTitle,
 };
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,15 +1,152 @@
 /* eslint-env mocha */
 
 import {
+	getResponsePageByTitle,
 } from '../../index.js';
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 chai.use( chaiAsPromised );
 
-describe( 'placeholder', () => {
+describe( 'getResponsePageByTitle', () => {
 
-	it( 'will be removed once real functionality exists', async () => {
-		expect( 2 + 2 ).to.equal( 4 );
+	it( 'finds page with same title, formatversion=1', () => {
+		const title = 'A Title';
+		const page = { title };
+		const unrelatedPage = { title: 'Unrelated' };
+		const response = { query: { pages: { 123: unrelatedPage, 456: page } } };
+		expect( getResponsePageByTitle( response, title ) ).to.equal( page );
+	} );
+
+	it( 'finds page with same title, formatversion=2', () => {
+		const title = 'A Title';
+		const page = { title };
+		const unrelatedPage = { title: 'Unrelated' };
+		const response = { query: { pages: [ unrelatedPage, page ] } };
+		expect( getResponsePageByTitle( response, title ) ).to.equal( page );
+	} );
+
+	it( 'does not find page with different title', () => {
+		const title = 'Title';
+		const page = { title };
+		const response = { query: { pages: [ page ] } };
+		const inputTitle = 'TiTlE';
+		expect( getResponsePageByTitle( response, inputTitle ) ).to.be.null;
+	} );
+
+	it( 'finds page with normalized title', () => {
+		const title = 'A Title';
+		const page = { title };
+		const inputTitle = 'a_Title';
+		const response = { query: {
+			normalized: [
+				{ from: 'unrelated', to: 'Unrelated' },
+				{ from: inputTitle, to: title },
+			],
+			pages: [ page ],
+		} };
+		expect( getResponsePageByTitle( response, inputTitle ) ).to.equal( page );
+	} );
+
+	it( 'finds page with redirected title', () => {
+		const title = 'Target';
+		const page = { title };
+		const inputTitle = 'Redirect';
+		const response = { query: {
+			redirects: [
+				{ from: 'Unrelated', to: 'Unrelated 2: The Unrelatening' },
+				{ from: inputTitle, to: title, tofragment: 'Ignored' },
+			],
+			pages: [ page ],
+		} };
+		expect( getResponsePageByTitle( response, inputTitle ) ).to.equal( page );
+	} );
+
+	it( 'finds page with double-redirected title', () => {
+		const title = 'Target';
+		const page = { title };
+		const inputTitle = 'Redirect';
+		const intermediateTitle = 'Redirect 2';
+		const response = { query: {
+			redirects: [
+				{ from: inputTitle, to: intermediateTitle },
+				{ from: intermediateTitle, to: title },
+			],
+			pages: [ page ],
+		} };
+		expect( getResponsePageByTitle( response, inputTitle ) ).to.equal( page );
+	} );
+
+	it( 'finds page with double-redirected title in flipped order', () => {
+		const title = 'Target';
+		const page = { title };
+		const inputTitle = 'Redirect';
+		const intermediateTitle = 'Redirect 2';
+		const response = { query: {
+			redirects: [
+				// could happen with titles: [ intermediateTitle, inputTitle ]
+				{ from: intermediateTitle, to: title },
+				{ from: inputTitle, to: intermediateTitle },
+			],
+			pages: [ page ],
+		} };
+		expect( getResponsePageByTitle( response, inputTitle ) ).to.equal( page );
+	} );
+
+	it( 'detects redirect loop', () => {
+		const redirect1 = 'Redirect 1';
+		const redirect2 = 'Redirect 2';
+		const redirect3 = 'Redirect 3';
+		const response = { query: { redirects: [
+			{ from: redirect1, to: redirect2 },
+			{ from: redirect2, to: redirect3 },
+			{ from: redirect3, to: redirect1 },
+		] } };
+		expect( getResponsePageByTitle( response, redirect1 ) ).to.be.null;
+	} );
+
+	it( 'detects redirect loop not involving input title', () => {
+		const redirect1 = 'Redirect 1';
+		const redirect2 = 'Redirect 2';
+		const redirect3 = 'Redirect 3';
+		const inputTitle = 'Here we go';
+		const response = { query: { redirects: [
+			{ from: inputTitle, to: redirect1 },
+			{ from: redirect1, to: redirect2 },
+			{ from: redirect2, to: redirect3 },
+			{ from: redirect3, to: redirect1 },
+		] } };
+		expect( getResponsePageByTitle( response, inputTitle ) ).to.be.null;
+	} );
+
+	it( 'finds page with normalized redirected title', () => {
+		const title = 'Target';
+		const page = { title };
+		const normalizedTitle = 'Redirect';
+		const inputTitle = 'redirect';
+		const response = { query: {
+			normalized: [
+				{ from: inputTitle, to: normalizedTitle },
+			],
+			redirects: [
+				{ from: normalizedTitle, to: title },
+			],
+			pages: [ page ],
+		} };
+		expect( getResponsePageByTitle( response, inputTitle ) ).to.equal( page );
+	} );
+
+	it( 'returns missing title', () => {
+		const title = 'No such page';
+		const page = { title, missing: true };
+		const response = { query: { pages: [ page ] } };
+		expect( getResponsePageByTitle( response, title ) ).to.equal( page );
+	} );
+
+	it( 'returns invalid title', () => {
+		const title = '<';
+		const page = { title, invalid: true };
+		const response = { query: { pages: [ page ] } };
+		expect( getResponsePageByTitle( response, title ) ).to.equal( page );
 	} );
 
 } );
